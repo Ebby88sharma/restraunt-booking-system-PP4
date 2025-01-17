acfc9b08ae278d508e8ea34b8719337aa75b627f
Abhinav Sharma <ab88sharma@gmail.com>
2025-01-17 12:04:32 +0000

    heroku check for the db issue

--- restaurant_booking/restaurant_booking/settings.py
+++ restaurant_booking/restaurant_booking/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'restaurant_booking.urls'
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'restaurant_booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # Correct path to the templates directory
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
