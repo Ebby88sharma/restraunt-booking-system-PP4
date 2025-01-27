fbbb1387aa9545cc0fb87a2ec3c5f5ce38f5b5fc
Abhinav Sharma <ab88sharma@gmail.com>
2025-01-27 01:24:17 +0000

    updated db issue resolved

--- restaurant_booking/restaurant_booking/settings.py
+++ restaurant_booking/restaurant_booking/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-gu84yp^44p8b!4#jleeu(q&uyz6^)n_htrtx=ljed(9+(&wux_'
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://restraunt-booking-system-pp4.onrender.com/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
